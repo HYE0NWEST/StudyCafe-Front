3bac0c67d3cedbc7eab25b0ede4b253378ed7c2a
Kim, Hyeonseo <hsys20020702@gmail.com>
2026-01-05 09:50:07 +0900

    front alaram

--- src/api/axios.js
+++ src/api/axios.js
@@ -14,20 +14,18 @@ instance.interceptors.response.use(
         if (err.response && err.response.data) {
             const data = err.response.data;
             
-            // 1. 서버가 보낸 데이터가 "JSON 객체"인 경우 (보기 싫은 그거)
+            
             if (typeof data === 'object') {
-                // message 필드가 있으면 그거 보여주고, 없으면 그냥 일반 문구 출력
-                // Spring Boot 기본 에러에는 message가 보통 포함되어 있음
+                
                 msg = data.message || "요청 처리 중 오류가 발생했습니다.";
             } 
-            // 2. 서버가 보낸 데이터가 그냥 "문자열"인 경우
+            
             else {
                 msg = data;
             }
         }
         
-        // JSON 덩어리는 절대 출력하지 않고, 정제된 msg만 출력
-        alert(msg);
+       
         return Promise.reject(err);
     }
 );
